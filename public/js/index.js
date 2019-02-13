2dbe619a55e2a922e8988dcbc9f1dbb832201494
Johan Poirier <johan.poirier@gmail.com>
2019-02-13 14:07:38 +0100

    Fix admin actions

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,15 @@
 import {addPlannedTask, removePlannedTask} from './modules/admin.js';
 
-document.querySelectorAll('button.admin-task').forEach(btn => {
+document.querySelectorAll('button.planning-task').forEach(btn => {
     btn.addEventListener('click', async event => {
         const {taskid, memberid, active} = event.target.dataset;
 
         if (active === 'false') {
             await addPlannedTask(parseInt(taskid, 10), parseInt(memberid,10));
-            event.target.classList.add('admin-task-active');
+            event.target.classList.add('planning-task-active');
         } else {
             await removePlannedTask(parseInt(taskid, 10), parseInt(memberid,10));
-            event.target.classList.remove('admin-task-active');
+            event.target.classList.remove('planning-task-active');
         }
     }, false);
 });
